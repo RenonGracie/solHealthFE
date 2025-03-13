a542c7c424736b536c204caae5d8f2299e4ecfb8
Serj Mileev <smileev@remedypointsolutions.com>
2025-03-13 16:05:11 +0300

    Prepare prod deploy (#31)

--- terraform/application.tf
+++ terraform/application.tf
@@ -1,13 +1,13 @@
 module "application" {
   source  = "cn-terraform/s3-static-website/aws"
   version = "1.0.9"
-  acm_certificate_arn_to_use = "arn:aws:acm:us-east-1:762233746762:certificate/49d6922d-be9b-47e1-a9f9-fd45f824e783"
+  acm_certificate_arn_to_use = var.acm_certificate_arn
   create_acm_certificate     = false
-  route53_hosted_zone_id     = "Z0565774GWNT1OIQP463"
+  route53_hosted_zone_id     = var.route53_hosted_zone_id
   create_route53_hosted_zone = false
   www_website_redirect_enabled = false
   name_prefix         = "solhealth"
-  website_domain_name = "dev.solhealth.co"
+  website_domain_name = var.domain_name
   cloudfront_custom_error_responses = [{
     error_caching_min_ttl = 10,
     error_code            = 404,
@@ -22,7 +22,7 @@ module "application" {
   tags = {
     "Owner"       = "Terraform"
     "Application" = "solhealth"
-    "Environment" = "dev"
+    "Environment" = var.environment
   }
   providers = {
     aws.main         = aws,
---
+++ terraform/outputs.tf
@@ -0,0 +1,4 @@
+output "cloudfront_distribution_id" {
+  description = "ID CloudFront Distribution"
+  value       = module.application.cloudfront_distribution_id
+} 
