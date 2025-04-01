a17cb9d45867ed4c6137afd480c43c64e66990f9
umarkau-remedy <umarkau@remedypointsolutions.com>
2025-04-01 10:40:30 +0300

    SHD-172: Make previous therapist photo square. (#76)

--- src/components/Steps/components/MatchedTherapist/components/PreviousTherapistCard/components/PreviousTherapistPhoto.tsx
+++ src/components/Steps/components/MatchedTherapist/components/PreviousTherapistCard/components/PreviousTherapistPhoto.tsx
@@ -1,9 +1,4 @@
-import * as React from 'react';
-
-import {
-  VIEWED_THERAPIST_PHOTO_SHAPES,
-  PLACEHOLDER_IMAGE_PATH,
-} from '@/constants';
+import { PLACEHOLDER_IMAGE_PATH } from '@/constants';
 
 export interface IPreviousTherapistPhotoProps {
   photoSrc?: string;
@@ -12,21 +7,8 @@ export interface IPreviousTherapistPhotoProps {
 export const PreviousTherapistPhoto = ({
   photoSrc,
 }: IPreviousTherapistPhotoProps) => {
-  const randomShape = React.useMemo(() => {
-    const randomIndex = Math.floor(
-      Math.random() * VIEWED_THERAPIST_PHOTO_SHAPES.length,
-    );
-
-    return `path('${VIEWED_THERAPIST_PHOTO_SHAPES[randomIndex]}')`;
-  }, []);
-
   return (
-    <div
-      className="w-[130px] h-[130px]"
-      style={{
-        clipPath: randomShape,
-      }}
-    >
+    <div className="w-[130px] h-[130px]">
       <img
         className="w-full h-full object-cover"
         src={photoSrc || PLACEHOLDER_IMAGE_PATH}
