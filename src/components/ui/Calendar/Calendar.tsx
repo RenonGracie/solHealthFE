84bdbf183825adabfe41057de16e5a1513cea4b9
Uladzimir <umarkau@remedypointsolutions.com>
2025-04-24 14:48:26 +0400

    SHD-156: Add hover effect for booking section buttons.

--- src/components/ui/Calendar/Calendar.tsx
+++ src/components/ui/Calendar/Calendar.tsx
@@ -24,10 +24,10 @@ export const Calendar = (props: DayPickerProps) => {
           'text-[var(--brand-black)] rounded-sm outline outline-1 outline-[var(--brand-brown)]',
         selected:
           'text-[var(--brand-black)] rounded-sm outline outline-1 outline-[var(--brand-brown)] bg-[var(--brand-yellow)]',
-        disabled: 'opacity-30',
+        disabled: 'opacity-30 pointer-events-none',
         [UI.Day]: 'font-normal text-sm tracking-[-0.02em]',
         [UI.DayButton]:
-          'hover:cursor-pointer w-[36px] h-[35px] lg:w-[42px] lg:h-[41px]',
+          'hover:cursor-pointer hover:bg-[var(--brand-coffee)] hover:rounded-sm hover:outline hover:outline-1 hover:outline-[var(--brand-brown)] w-[36px] h-[35px] lg:w-[42px] lg:h-[41px]',
         [UI.Weekday]: 'font-normal text-base opacity-70 tracking-[-0.02em]',
       }}
       {...props}
