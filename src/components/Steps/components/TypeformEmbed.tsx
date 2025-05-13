8936675ac80c8a685ee7b2f6c39cb73a6b8fb511
umarkau-remedy <umarkau@remedypointsolutions.com>
2025-05-13 11:25:28 +0300

    SHD-245: Attach client id and utm params to typeform. (#141)

--- src/components/Steps/components/TypeformEmbed.tsx
+++ src/components/Steps/components/TypeformEmbed.tsx
@@ -1,12 +1,42 @@
 import * as React from 'react';
 import { Widget } from '@typeform/embed-react';
+
 import { GTM_EVENTS, trackEvent } from '@/lib/gtm';
+import { getClientIdFromGaCookie } from '@/lib/ga';
+import { getUtmParams } from '@/lib/utm';
+import { TYPEFORM_HIDDEN_FIELDS } from '@/constants';
 
 interface IProps {
   onSubmit: (responseId: string) => void;
 }
 
 export const TypeformEmbed: React.FC<IProps> = ({ onSubmit }) => {
+  const [clientId, setClientId] = React.useState('');
+
+  const utmParams = React.useMemo(() => getUtmParams(), []);
+
+  React.useEffect(() => {
+    trackEvent(GTM_EVENTS.FORM_STARTED);
+
+    let id = getClientIdFromGaCookie();
+
+    if (id) {
+      setClientId(id);
+      return;
+    }
+
+    const interval = setInterval(() => {
+      id = getClientIdFromGaCookie();
+
+      if (id) {
+        setClientId(id);
+        clearInterval(interval);
+      }
+    }, 200);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <Widget
       id={import.meta.env.VITE_TYPEFORM_ID as string}
@@ -17,6 +47,10 @@ export const TypeformEmbed: React.FC<IProps> = ({ onSubmit }) => {
       onSubmit={({ responseId }) => {
         trackEvent(GTM_EVENTS.FORM_SUBMITTED);
         onSubmit(responseId);
+      }}
+      hidden={{
+        [TYPEFORM_HIDDEN_FIELDS.CLIENT_ID]: clientId,
+        ...utmParams,
       }}
     />
   );
--- src/lib/gtm.ts
+++ src/lib/gtm.ts
@@ -1,5 +1,7 @@
 import TagManager from 'react-gtm-module';
 
+import { getUtmParams } from './utm';
+
 export const GTM_EVENTS = {
   FORM_STARTED: 'FormStarted',
   FORM_SUBMITTED: 'FormSubmitted',
@@ -13,9 +15,12 @@ export const trackEvent = (
   eventData?: Record<string, unknown>,
 ) => {
   if (typeof window !== 'undefined') {
+    const utmParams = getUtmParams();
+
     TagManager.dataLayer({
       dataLayer: {
         event: eventName,
+        ...utmParams,
         ...eventData,
       },
     });
